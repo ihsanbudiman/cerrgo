878e4578c6f3f63f94b0677f18556dbd66043de6
Moh Ihsan Budiman <ninoidgt@gmail.com>
2023-06-21 14:48:20 +0700

    update response writer

--- error.go
+++ error.go
@@ -1,5 +1,10 @@
 package cerrgo
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Error struct {
 	Code    int
 	Message string
@@ -9,13 +14,13 @@ func (e *Error) Error() string {
 	return e.Message
 }
 
-func IsCustomError(err error) bool {
+func isCustomError(err error) bool {
 	_, ok := err.(*Error)
 	return ok
 }
 
 func GetErrResponse(err error) map[string]interface{} {
-	if IsCustomError(err) {
+	if isCustomError(err) {
 		return map[string]interface{}{
 			"code":    err.(*Error).Code,
 			"message": err.(*Error).Message,
@@ -33,4 +38,31 @@ func NewError(code int, message string) error {
 		Code:    code,
 		Message: message,
 	}
+}
+
+func getErrCode(err error) int {
+	if isCustomError(err) {
+		return err.(*Error).Code
+	}
+
+	return 500
+}
+
+func SendResponse(w http.ResponseWriter, err error) {
+
+	errData := GetErrResponse(err)
+	jsonData, err := json.Marshal(errData)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(`{"code":500,"message":"Internal Server Error"}`))
+		return
+	}
+
+	// check if w.header is not set
+	if w.Header().Get("Content-Type") == "" {
+		w.Header().Set("Content-Type", "application/json")
+	}
+
+	w.WriteHeader(getErrCode(err))
+	w.Write(jsonData)
 }
